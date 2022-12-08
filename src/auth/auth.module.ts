3edf76ee02ce8ce017d991d9eb9a4670e39ddf1e
George Armando <georgearmando54@gmail.com>
2022-12-08 13:18:04 +0100

    improves folder structure

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,9 @@ import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './auth.service';
-import { LocalStrategy } from './local.strategy';
 import { jwtConstants } from './constants';
-import { JwtStrategy } from './jwt.strategy';
+import { LocalStrategy } from './passport/strategies/local.strategy';
+import { JwtStrategy } from './passport/strategies/jwt.strategy';
 
 @Module({
   imports: [
